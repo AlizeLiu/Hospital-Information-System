61f457e7d5af1d40a7c5b5b3f3b44625ea09dc28
Alize.Liu <3152067813@qq.com>
2023-10-21 11:30:59 +0800

    修改数据库文件

--- backend/db_mysql/mySqlTest.go
+++ backend/db_mysql/mySqlTest.go
@@ -10,10 +10,10 @@ var dsn = "root:ll3152067813@tcp(127.0.0.1:3306)/HiS_db"
 
 func InitDB() {
 	//数据库
-	//用户名:密码@tcp(ip:端口)/数据库的名字
+	//用户名:密码@tcp(ip:3306)/数据库的名字
 	//dsn := "root:ll3152067813@tcp(127.0.0.1:3306)/HiS_db"
 	//连接数据集
-	db, err := sql.Open("mysql", dsn) //open不会检验用户名和密码
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Printf("dsn:%s invalid,err:%v\n", dsn, err)
 		return
@@ -23,30 +23,57 @@ func InitDB() {
 		fmt.Printf("open %s faild,err:%v\n", dsn, err)
 		return
 	}
-	fmt.Println("连接数据库成功")
+	fmt.Println("connect success")
 	db.SetMaxIdleConns(10)
 	return
 }
 
-func Insert(usr, passwd, role string) error {
+/*
+
+
+	User表操作
+
+
+*/
+
+func SqlInsert_User(usr, passwd, role string) error {
 	db, err := sql.Open("mysql", dsn)
 
 	if err != nil {
 		return err
 	}
 	defer db.Close()
-	InsertsqlStr := "insert into user(Username,Password,Role) VALUES(?,?,?)"
-	_, err = db.Exec(InsertsqlStr, usr, passwd, role)
-
+	InsertUser := "insert into user(Username,Password,Role) VALUES(?,?,?)" //写user表
+	_, err = db.Exec(InsertUser, usr, passwd, role)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%v create successful", usr)
+	var userID int
+	err = db.QueryRow("SELECT LAST_INSERT_ID()").Scan(&userID)
+	InsertUserRole := "INSERT INTO UserRole (UserID, Role) VALUES (?, ?)"
+	_, err = db.Exec(InsertUserRole, userID, role) //关联userrole表
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v create successful\n", usr)
 	return nil
 }
 
+func sqldelete_User() {
+
+}
+
+func SqlUpdate_User() {
+
+}
+
+func SqlQuery_User() {
+
+}
+
 /*
-func main() {
+仅测试 不使用
+_func main() {
 	err := Insert("全志愿", "000000", "Doctor")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
